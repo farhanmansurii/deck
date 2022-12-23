ceda645c5071aee62567afb0ee590471c8a42225
Farhan <mansurifarhanfm@gmail.com>
2022-12-23 20:35:02 +0530

    hi

--- deck-frontend/src/App.tsx
+++ deck-frontend/src/App.tsx
@@ -1,12 +1,55 @@
+import { Input } from '@nextui-org/react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
+  const [decks, setdecks] = useState([])
+  const [title, settitle] = useState()
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault()
+    await fetch('http://localhost:5000/deck', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+  useEffect(() => {
+    async function getDecks() {
+      axios.get('http://localhost:5000/deck').then((response) => {
+        setdecks(response.data)
+      })
+      // axios.get('http://localhost:5000/decks').then((response) => setdecks(response.json()))
+    }
+    return () => {
+      getDecks()
+    }
+  }, [])
+
   return (
-    <div className="text-3xl font-semibold font-sans  flex-col flex">
+    <div className="text-3xl font-semibold font-sans  w-9/12 mx-auto py-10 space-y-10 flex-col flex">
       <div className="text-3xl font-semibold ">CardDeck</div>
       <div></div>
-      <div className="gap-3 w-full my-10  text-start ">
+      <div className="gap-3 w-full my-10 space-y-10   text-start ">
+        <form onSubmit={handleCreateDeck}>
+          <Input
+            size="lg"
+            value={title}
+            placeholder="Enter deck name"
+            width="400px"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              settitle(e.target.value)
+            }}
+          />
+        </form>
+        {decks.map((deck: any) => (
+          <div>{deck.title}</div>
+        ))}
         <Card />
       </div>
     </div>
--- services/src/index.ts
+++ services/src/index.ts
@@ -1,9 +1,27 @@
-import express from 'express'
+import cors from 'cors'
+import express, { Request, Response } from 'express'
 import mongoose from 'mongoose'
+import DeckModel from './Schema/Deck'
 const app = express()
-
-app.use('/', (req, res) => {
+app.use(
+  cors({
+    origin: '*',
+  })
+)
+app.get('/', (req: Request, res: Response) => {
   res.send('WelcomeBoy')
+})
+app.use(express.json())
+app.post('/deck', async (req: Request, res: Response) => {
+  const newDeck = new DeckModel({
+    title: req.body.title,
+  })
+  const savedDeck = await newDeck.save()
+  res.json(savedDeck)
+})
+app.get('/deck', async (req: Request, res: Response) => {
+  const Decks = await DeckModel.find()
+  res.json(Decks)
 })
 mongoose.connect('mongodb+srv://farhan:farhan@cluster0.23dxfen.mongodb.net/?retryWrites=true&w=majority').then(() => {
   console.log('connection established')
