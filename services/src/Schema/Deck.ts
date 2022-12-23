60ff341bfdb2e84db33a179a8d9cceab015b1d92
Farhan <mansurifarhanfm@gmail.com>
2022-12-23 20:35:06 +0530

    s

---
+++ services/src/Schema/Deck.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+const ObjectID = mongoose.Types.ObjectId
+const Deck = new Schema({
+  title: String,
+})
+const DeckModel = mongoose.model('Deck', Deck)
+
+export default DeckModel
