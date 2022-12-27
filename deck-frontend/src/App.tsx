6c77dea88a47b6fb827d9695ea0565f6f71b1387
Farhan <mansurifarhanfm@gmail.com>
2022-12-27 15:16:44 +0530

    hi

--- deck-frontend/src/App.tsx
+++ deck-frontend/src/App.tsx
@@ -1,125 +1,102 @@
-import axios from 'axios';
-import { motion, useAnimation } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
-import Spinner from 'react-spinner-material';
-import './App.css';
+import { motion, useAnimation } from 'framer-motion'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Spinner from 'react-spinner-material'
+import { createDeck } from './api/createDeck'
+import { deleteDeck } from './api/deleteDecks'
+import { getDecks, TDeck } from './api/getDecks'
+import './App.css'
 function App() {
-  const [decks, setdecks] = useState([])
+  const [decks, setdecks] = useState<TDeck[]>([])
   const [loading, setLoading] = useState(false)
   const [isloading, setIsLoading] = useState(false)
   const [status, setStatus] = useState<string | undefined>()
   const [title, settitle] = useState<string | undefined>()
-  const ripple = useAnimation();
+  const ripple = useAnimation()
 
   const startRipple = () => {
     ripple.start({
       scale: 1,
-      transition: { duration: 0.5 }
-    });
-  };
+      transition: { duration: 0.5 },
+    })
+  }
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault()
-    setIsLoading(true)
-    if (title?.trim().length ?? 1 > 0) {
-
-      await fetch('https://dex-api.vercel.app/deck', {
-        method: 'POST',
-        body: JSON.stringify({
-          title,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).then(response => { settitle(''), getDecks(), setIsLoading(false) },)
-    }
+    const deck = await createDeck(title)
+    setdecks([...decks, deck])
+    settitle('')
   }
   async function deleteDecks(id: any) {
-    setIsLoading(true)
-    await axios.delete(`https://dex-api.vercel.app/deck/${id}`).then(res => {
-      console.log(res)
-      setStatus("Deck Deleted")
-      getDecks()
-      setLoading(false)
-      setIsLoading(false)
-    })
+    setdecks(decks.filter((deck) => deck._id !== id))
+    await deleteDeck(id)
+  }
 
-  }
-  async function getDecks() {
-    setIsLoading(true)
-    await axios.get('https://dex-api.vercel.app/deck').then((response) => {
-      setdecks(response.data)
-    })
-    setIsLoading(false)
-  }
   useEffect(() => {
-    setIsLoading(true)
-    getDecks()
+    async function fetchDecks() {
+      const newDecks = await getDecks()
+      setdecks(newDecks)
+    }
+    fetchDecks()
   }, [])
 
   return (
-    <motion.div
-      initial={{ scale: 0.8 }}
-
-      animate={{ scale: 1 }}
-      transition={{ duration: 0.25 }}
-    >
+    <motion.div initial={{ scale: 0.8 }} animate={{ scale: 1 }} transition={{ duration: 0.25 }}>
       <div className=" font-sans min-h-screen  bg-[#363636] text-[#bebebe] w-11/12 lg:w-9/12 mx-auto py-10 flex-col flex">
         <div className=" text-5xl lg:text-7xl my-2  capitalize">All Boards</div>
 
         <div className="gap-3 w-full mb-10   text-start ">
-          <form onSubmit={handleCreateDeck} className='my-3  flex flex-row justify-between'>
-
-            <input autoComplete='true'
+          <form onSubmit={handleCreateDeck} className="my-3  flex flex-row justify-between">
+            <input
+              autoComplete="true"
               value={title}
               placeholder="Enter Board Name"
-              className='w-10/12 p-2 focus:border-b-2 focus:border-[#bebebe] placeholder:text-[#bebebe] text-xl bg-inherit border-0 focus:outline-none'
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                settitle(e.target.value)
-              }
+              className="w-10/12 p-2 focus:border-b-2 focus:border-[#bebebe] placeholder:text-[#bebebe] text-xl bg-inherit border-0 focus:outline-none"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => settitle(e.target.value)}
             />
-            <button className='bg-[#bebebe] hover:text-[#bebebe] w-fit hover:bg-[#363636] hover:border-[#bebebe] border-[#363636] border-2 text-[#363636] p-3 hover: duration-200 hover:scale-95 rounded-full border-3 '>
+            <button className="bg-[#bebebe] hover:text-[#bebebe] w-fit hover:bg-[#363636] hover:border-[#bebebe] border-[#363636] border-2 text-[#363636] p-3 hover: duration-200 hover:scale-95 rounded-full border-3 ">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
               </svg>
-
             </button>
           </form>
 
-          {
-            isloading ? (
-
-              <div className='w-full grid justify-center h-[200px] '>
-                <Spinner radius={30} color={"#bebebe"} stroke={2} visible={true} className='my-auto' />
-              </div>
-            ) : (
-
-
-
-              <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
-                {decks.length > 0 ? decks.reverse().map((deck: any) => (
-
-                  <div key={deck._id} className=' min-h-[100px] break-all lg:aspect-video text-2xl items-center  flex justify-between   hover:scale-[98%] duration-150  hover:bg-[#bebebe]/80 rounded-xl text-[#363636] bg-[#bebebe]' >
-                    <Link to={`/deck/${deck._id}`} className=' px-5 capitalize text-[#363636]'>
+          {isloading ? (
+            <div className="w-full grid justify-center h-[200px] ">
+              <Spinner radius={30} color={'#bebebe'} stroke={2} visible={true} className="my-auto" />
+            </div>
+          ) : (
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
+              {decks.length > 0 ? (
+                decks.reverse().map((deck: any) => (
+                  <div
+                    key={deck._id}
+                    className=" min-h-[100px] break-all lg:aspect-video text-2xl items-center  flex justify-between   hover:scale-[98%] duration-150  hover:bg-[#bebebe]/80 rounded-xl text-[#363636] bg-[#bebebe]"
+                  >
+                    <Link to={`/deck/${deck._id}`} className=" px-5 capitalize text-[#363636]">
                       {deck.title}
                     </Link>
 
-                    <div className='p-3' onClick={() =>
-                      deleteDecks(deck._id)
-                    }>
-
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 hover:text-red-500 duration-150 hover:animate-pulse rounded-full  h-6">
+                    <div className="p-3" onClick={() => deleteDecks(deck._id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 hover:text-red-500 duration-150 hover:animate-pulse rounded-full  h-6"
+                      >
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                       </svg>
-
                     </div>
-
                   </div>
-                )) : (<div className=' text-center text-3xl mt-10 mx-auto flex w-10/12 '> No Decks :/</div>)}
-              </div>
-            )}
+                ))
+              ) : (
+                <div className=" text-center text-3xl mt-10 mx-auto flex w-10/12 "> No Decks :/</div>
+              )}
+            </div>
+          )}
         </div>
-      </div >
+      </div>
     </motion.div>
   )
 }
--- deck-frontend/src/components/Deck.tsx
+++ deck-frontend/src/components/Deck.tsx
@@ -1,128 +1,120 @@
-import axios from 'axios';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { Link, useParams } from "react-router-dom";
-import Spinner from 'react-spinner-material';
+import axios from 'axios'
+import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { createCard } from '../api/createCard'
+import { deleteCard } from '../api/deleteCard'
+import { getDeck } from '../api/getDeck'
+import { TDeck } from '../api/getDecks'
 function Deck() {
   const { deckId } = useParams()
-  const [card, setcard] = useState<any[]>([])
+  const [card, setcard] = useState<string[]>([])
   const [isloading, setIsLoading] = useState(true)
-  const [deck, setDeck] = useState<any[]>([])
-  const [loading, setLoading] = useState(false)
-  const [status, setStatus] = useState<string | undefined>()
+  const [deck, setDeck] = useState<TDeck | undefined>()
   const [text, setText] = useState<string | undefined>()
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault()
-    setIsLoading(true)
-    if (text?.trim().length ?? 1 > 0) {
-
-      await fetch(`https://dex-api.vercel.app/deck/${deckId}/card`, {
-        method: 'POST',
-        body: JSON.stringify({
-          text
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).then(response => { console.log(response), getDecks(), setText(''), setStatus("New Deck Created") },)
-    }
-    setIsLoading(false)
+    const { cards: serverCards } = await createCard(deckId!, text)
+    setcard(serverCards)
+    setText('')
   }
 
-  async function deleteCard(deckId: any, index: number) {
-    setIsLoading(true)
-
-    if (!deckId) return;
-    await axios.delete(`https://dex-api.vercel.app/deck/${deckId}/card/${index}`).then(res => console.log(res))
-    await getDecks()
-    setIsLoading(false)
+  async function deleteCards(deckId: any, index: number) {
+    if (!deckId) return
+    const newDeck = await deleteCard(deckId, index)
+    setcard(newDeck.cards)
   }
   async function getDecks() {
     setIsLoading(true)
     await axios.get(`https://dex-api.vercel.app/deck/${deckId}/card`).then((response) => {
       setcard(response.data.cards)
-      setDeck(response.data.title)
-
+      setDeck(response.data)
     })
     setIsLoading(false)
   }
   useEffect(() => {
-    setIsLoading(true)
-    getDecks()
-    setIsLoading(false)
-  }, [])
+    async function fetchDeck() {
+      if (!deckId) return
+      const newDeck = await getDeck(deckId)
+      setDeck(newDeck)
+      setcard(newDeck.cards)
+    }
 
+    fetchDeck()
+  }, [deckId])
   return (
-    <motion.div
-      initial={{ scale: 0.8 }}
-      animate={{ scale: 1 }}
-      transition={{ duration: 0.3 }}
-    >
-
-      <div className='w-full min-h-screen  text-[#363636] bg-[#bebebe]'>
+    <motion.div initial={{ scale: 0.8 }} animate={{ scale: 1 }} transition={{ duration: 0.3 }}>
+      <div className="w-full min-h-screen  text-[#363636] bg-[#bebebe]">
         (
         <div className=" font-sans  w-11/12 lg:w-9/12 mx-auto py-10  flex-col flex">
-
           <div className="text-5xl gap-5 lg:text-7xl flex  my-2 capitalize ">
-            <Link className='text-inherit grid items-center ' to='/'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12  duration-100 hover:border-2  rounded-full border-[#333] p-2  h-12">
+            <Link className="text-inherit grid items-center " to="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-12  duration-100 hover:border-2  rounded-full border-[#333] p-2  h-12"
+              >
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
               </svg>
-
             </Link>
-            <div>
-              {deck}
-            </div>
+            {deck?.title}
           </div>
 
           <div className="gap-3 w-full mb-10   text-start ">
-            <form onSubmit={handleCreateDeck} className='my-3  flex flex-row justify-between'>
-
-              <input autoComplete='true'
+            <form onSubmit={handleCreateDeck} className="my-3  flex flex-row justify-between">
+              <input
+                autoComplete="true"
                 value={text}
                 placeholder="Enter Card Name"
-                className='w-9/12 p-2 focus:border-b-2 focus:border-[#363636] text-xl  placeholder:text-[#363636] bg-inherit border-0 focus:outline-none'
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setText(e.target.value)
-                }
+                className="w-9/12 p-2 focus:border-b-2 focus:border-[#363636] text-xl  placeholder:text-[#363636] bg-inherit border-0 focus:outline-none"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
               />
-              <button className='text-[#bebebe]  hover:bg-[#bebebe] w-fit hover:text-[#363636] border-[#bebebe] hover:border-[#363636] border-2 bg-[#363636] p-3 hover: duration-200  rounded-full border-3 '>
+              <button className="text-[#bebebe]  hover:bg-[#bebebe] w-fit hover:text-[#363636] border-[#bebebe] hover:border-[#363636] border-2 bg-[#363636] p-3 hover: duration-200  rounded-full border-3 ">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
-
               </button>
             </form>
-            {
-              isloading ? (
 
-                <div className='w-full grid justify-center h-[200px] '>
-                  <Spinner radius={30} color={"#333"} stroke={2} visible={true} className='my-auto' />
-                </div>
-              ) :
-                <div className="grid  lg:grid-cols-4 gap-2">
-                  {card.length > 0 ?
-                    card.map((e: string, index: number) =>
-                      <div key={index} className=' min-h-[100px] lg:aspect-video text-xl items-center  flex justify-between rounded-xl lg:text-2xl hover:scale-[98%] duration-150  hover:bg-[#363636]/80 bg-[#363636] text-[#bebebe]' >
-                        <div className=' whitespace-pre-wrap p-2 w-fit text-clip text-2xl break-all  text-[#bebebe]'>
-                          <div className=' px-3 capitalize my-auto'>
-                            {e}
-                          </div>
-                        </div>
-                        <div className='p-3' onClick={() => {
-                          deleteCard(deckId, index)
-                        }}>
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 hover:text-red-500 duration-150 hover:animate-pulse rounded-full  h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                          </svg>
-
-
-                        </div>
-                      </div>) : (<div className=' text-center text-3xl mt-10 mx-auto flex w-10/12 '> No Cards :/</div>)
-                  }
-                </div>}
+            <div className="grid  lg:grid-cols-4 gap-2">
+              {card.length > 0 ? (
+                card.map((e: string, index: number) => (
+                  <div
+                    key={index}
+                    className=" min-h-[100px] lg:aspect-video text-xl items-center  flex justify-between rounded-xl lg:text-2xl hover:scale-[98%] duration-150  hover:bg-[#363636]/80 bg-[#363636] text-[#bebebe]"
+                  >
+                    <div className=" whitespace-pre-wrap p-2 w-fit text-clip text-2xl break-all  text-[#bebebe]">
+                      <div className=" px-3 capitalize my-auto">{e}</div>
+                    </div>
+                    <div
+                      className="p-3"
+                      onClick={() => {
+                        deleteCards(deckId, index)
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 hover:text-red-500 duration-150 hover:animate-pulse rounded-full  h-6"
+                      >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div className=" text-center text-3xl mt-10 mx-auto flex w-10/12 "> No Cards :/</div>
+              )}
+            </div>
           </div>
-        </div >)
+        </div>
+        )
       </div>
     </motion.div>
   )
